440b25bed44765f49ad7ff7cc7a54a5ef111f9e0
Roland Gsell <roland.gsell@gmx.net>
2018-05-07 07:07:38 +0200

    printing out the new filenames

--- rename.py
+++ rename.py
@@ -16,7 +16,7 @@ def creation_date(path_to_file):
             return stat.st_mtime
 
 
-mypath = '/home/roland/git/rename'
+mypath = '/home/roland/git/rename/'
 files = []
 for (dirpath, dirnames, filenames) in os.walk(mypath):
     files.extend(filenames)
@@ -24,10 +24,21 @@ for (dirpath, dirnames, filenames) in os.walk(mypath):
 
 filelist = {}
 for item in files:
-    item = mypath + os.sep + item
+    if mypath[-1] is not os.sep:
+        item = mypath + os.sep + item
+    else:
+        item = mypath + item
     print(creation_date(item), ': ', item)
     filelist.update({creation_date(item): item})
 
 ordered_filelist = collections.OrderedDict(sorted(filelist.items()))
 print(str(files))
 print(str(ordered_filelist))
+
+for i in range(len(ordered_filelist)):
+    prefix = str(i).zfill(3)
+    (modification, filename) = list(ordered_filelist.items())[i]
+    newfilename = filename.rsplit(os.sep, 1)
+    newfilename[-1] = prefix + '_' + newfilename[-1]
+    newfilename = newfilename[0] + os.sep + newfilename[-1]
+    print(newfilename)
